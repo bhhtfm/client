e6c4dc93fe2ce1a346123fb150bde401fb3e98a8
bhhtfm <bhhyilmaz@gmail.com>
2025-01-03 18:25:51 +0300

    sdasd

--- bin/hotkey.py
+++ bin/hotkey.py
@@ -1,21 +1,16 @@
 import time
 import json
+import random
 from pynput.keyboard import Key, Listener, Controller
 
 keyboard = Controller()
 
-# GET ROOM FROM FILE
 with open('vanilla.json', 'r') as file:
     data = json.load(file)
 
 rooms = [item for item in data if item['num_players'] > 10]
 
-# HOTKEY
-room_index = 0
-
 def on_press(key):
-    global room_index
-
     if key == Key.f1:
         keyboard.press(Key.enter)
         keyboard.type("/mort")
@@ -32,14 +27,10 @@ def on_press(key):
         keyboard.press(Key.enter)
 
     if key == Key.delete:
-        if room_index > len(rooms):
-            room_index = 0
-
-        room_name = rooms[room_index]['name']
+        room_name = random.choice(rooms)['name']
         keyboard.press(Key.enter)
         keyboard.type(f"/sala {room_name}")
         keyboard.press(Key.enter)
-        room_index += 1
 
 with Listener(on_press=on_press) as listener:
-    listener.join()  
+    listener.join()
