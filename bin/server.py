120ce3e9137789fc928d303993e6e0ae7e26fd50
bhhtfm <bhhyilmaz@gmail.com>
2025-01-04 02:43:49 +0300

    asdasdasd

--- bin/server.py
+++ bin/server.py
@@ -1,24 +1,53 @@
 import caseus
 import pak
 import json
+import re
 
 class MyProxy(caseus.proxies.LoggingProxy, caseus.proxies.InputListeningProxy):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.object_id = None
+        self.shaman_object_id = None
+        self.round_id = None
+
     # BLOCK INVENTORY ITEMS
     @pak.packet_listener(caseus.clientbound.AddShamanObjectPacket)
     async def fetch_object(self, source, packet):
-        if packet.shaman_object_id in {6, 0, 34, 65, 89, 33, 95, 97}:
-            await source.destination.write_packet(
-                caseus.clientbound.ObjectSyncPacket,
-                 
-                objects = [
-                    caseus.ClientboundObjectInfo(
-                        object_id = packet.object_id,
-                        shaman_object_id = -1,
-                    ),
-                ],        
-            )
-            return self.REPLACE_PACKET 
+        self.object_id = packet.object_id
+        self.shaman_object_id = packet.shaman_object_id 
 
+        await source.destination.write_packet(
+            caseus.clientbound.AddShamanObjectPacket,
+
+            object_id = packet.object_id,
+            shaman_object_id = -1,
+            mice_collidable=False,
+        )
+        return self.REPLACE_PACKET
+
+    @pak.packet_listener(caseus.clientbound.ObjectSyncPacket)
+    async def get_sync_object(self, source, packet):
+        for object in packet.objects:
+            if object.shaman_object_id == self.shaman_object_id:
+                await source.destination.write_packet(
+                    caseus.clientbound.ObjectSyncPacket,
+
+                     objects = [
+                         caseus.ClientboundObjectInfo(
+                             object_id = self.object_id,
+                             shaman_object_id = 10,
+                         )
+                     ]
+                )
+                return self.REPLACE_PACKET
+
+ #   @pak.packet_listener(caseus.clientbound.LegacyWrapperPacket)
+ #   async def get_legacy(self, source, packet):
+ #       await source.destination.write_packet(
+ #           caseus.clientbound.LegacyWrapperPacket,
+ #       )
+ #       return self.REPLACE_PACKET   
+        
     # REQUEST ROOM LIST
     @pak.packet_listener(caseus.clientbound.RoomListPacket)
     async def save_room(self, source, packet):
