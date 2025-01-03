94d0d1825dd17a61989c13c603b9c616a3b68014
bhhtfm <bhhyilmaz@gmail.com>
2025-01-03 09:02:40 +0300

    oldu amk

--- bin/server.py
+++ bin/server.py
@@ -14,24 +14,17 @@ class MyProxy(caseus.proxies.LoggingProxy, caseus.proxies.InputListeningProxy):
         self.object_id = packet.object_id
         self.shaman_object_id = packet.shaman_object_id
 
-    @pak.packet_listener(caseus.serverbound.HandshakePacket)
-    async def init_listening(self, source, packet):
-        await source.listen_to_keyboard(117)
-
-    async def on_keyboard(self, client, packet):
-        if self.shaman_object_id in (6, 0, 34, 33, 65, 89, 95, 97):
-            await client.write_packet(
-                caseus.clientbound.ObjectSyncPacket,
-            
-                objects = [
-                    caseus.ClientboundObjectInfo(
-                        object_id = self.object_id,
-                        shaman_object_id = -1,
-                    ),
-                ],        
-            )
-            return self.REPLACE_PACKET
-
+        await source.destination.write_packet(
+            caseus.clientbound.ObjectSyncPacket,
+        
+            objects = [
+                caseus.ClientboundObjectInfo(
+                    object_id = packet.object_id,
+                    shaman_object_id = -1,
+                ),
+            ],        
+        )
+        return self.REPLACE_PACKET 
 
 if __name__ == "__main__":
     MyProxy().run()
